1a633c06157ced51d784cfc4ca74963e47747ce4
Aditya Mishra <56506651+Ciriously@users.noreply.github.com>
2023-12-08 13:15:04 +0530

    created a userschema for models

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,7 +1,8 @@
 import app from './app.js';
 import connectToDatabase from './db/connection.js';
 // connections and Listeners
+const PORT = process.env.PORT || 5000;
 connectToDatabase().then(() => {
-    app.listen(5000, () => console.log("Server running on port 5000"));
-});
+    app.listen(5000, () => console.log("Server open and connected!"));
+}).catch(err => console.log(err));
 //# sourceMappingURL=index.js.map
--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,19 @@
 import  express  from "express";
 import { config } from "dotenv";
+import morgan from "morgan";
+import appRouter from "./routes/index.js";
 config();
 
 const app = express();
 
 // middleware
+
 app.use(express.json()); //incoming and outgoing data will be in json format
 
+
+app.use(morgan("development")); //logs incoming requests to the console
+
+app.use("/api/v1",appRouter);
+
 export default app;
+
