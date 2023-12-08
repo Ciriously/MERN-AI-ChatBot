66a2a750b0b866ee7dfd4f17e0f69a7870a8d0ce
Aditya Mishra <56506651+Ciriously@users.noreply.github.com>
2023-12-08 23:05:47 +0530

    created the frontend

---
+++ frontend/src/main.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { createTheme, ThemeProvider } from "@mui/material";
+
+const theme = createTheme({ typography: { fontFamily: "Poppins" } });
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
