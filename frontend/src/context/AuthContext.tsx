d37548eebca366ce5f2691d8cf84a3370cafd582
Aditya Mishra <56506651+Ciriously@users.noreply.github.com>
2023-12-09 17:43:08 +0530

    added login page and routing

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -5,6 +5,12 @@ import {
   useEffect,
   useState,
 } from "react";
+import {
+  checkAuthStatus,
+  loginUser,
+  logoutUser,
+  signupUser,
+} from "../helpers/api-communicator";
 
 type User = {
   name: string;
@@ -25,18 +31,42 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
 
   useEffect(() => {
     // fetch if the user's cookies are valid then skip login
+    async function checkStatus() {
+      const data = await checkAuthStatus();
+      if (data) {
+        setUser({ email: data.email, name: data.name });
+        setIsLoggedIn(true);
+      }
+    }
+    checkStatus();
   }, []);
-
-  const login = async (email: string, password: string) => {};
-  const signup = async (name: string, email: string, password: string) => {};
-  const logout = async () => {};
+  const login = async (email: string, password: string) => {
+    const data = await loginUser(email, password);
+    if (data) {
+      setUser({ email: data.email, name: data.name });
+      setIsLoggedIn(true);
+    }
+  };
+  const signup = async (name: string, email: string, password: string) => {
+    const data = await signupUser(name, email, password);
+    if (data) {
+      setUser({ email: data.email, name: data.name });
+      setIsLoggedIn(true);
+    }
+  };
+  const logout = async () => {
+    await logoutUser();
+    setIsLoggedIn(false);
+    setUser(null);
+    window.location.reload();
+  };
 
   const value = {
-    isLoggedIn,
     user,
+    isLoggedIn,
     login,
-    signup,
     logout,
+    signup,
   };
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 };
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -5,6 +5,7 @@ import CustomizedInput from "../components/shared/CustomizedInput";
 import { toast } from "react-hot-toast";
 import { useAuth } from "../context/AuthContext";
 import { useNavigate } from "react-router-dom";
+
 const Login = () => {
   const navigate = useNavigate();
   const auth = useAuth();
@@ -30,7 +31,7 @@ const Login = () => {
   return (
     <Box width={"100%"} height={"100%"} display="flex" flex={1}>
       <Box padding={8} mt={8} display={{ md: "flex", sm: "none", xs: "none" }}>
-        <img src="robot.png" alt="Robot" style={{ width: "400px" }} />
+        <img src="robot.png" alt="login" style={{ width: "400px" }} />
       </Box>
       <Box
         display={"flex"}
