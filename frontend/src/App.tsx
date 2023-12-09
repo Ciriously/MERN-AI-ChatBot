fe12856501c0c453bf69ec9ab56c756c48cc44c7
Aditya Mishra <56506651+Ciriously@users.noreply.github.com>
2023-12-09 12:01:00 +0530

    added Auth using context

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,8 +5,10 @@ import { Login } from "./pages/Login";
 import { Signup } from "./pages/Signup";
 import { Chat } from "./pages/Chat";
 import { NotFound } from "./pages/NotFound";
+import { UserAuth } from "./context/AuthContext";
 
 function App() {
+  console.log(UserAuth()?.isLoggedIn);
   return (
     <main>
       <Header />
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,15 +4,18 @@ import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { createTheme, ThemeProvider } from "@mui/material";
+import { AuthProvider } from "./context/AuthContext.tsx";
 
 const theme = createTheme({ typography: { fontFamily: "Poppins" } });
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <App />
-      </ThemeProvider>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <App />
+        </ThemeProvider>
+      </BrowserRouter>
+    </AuthProvider>
   </React.StrictMode>
 );
