583712a1184938e58185999756235e088d0b2492
Aditya Mishra <56506651+Ciriously@users.noreply.github.com>
2023-12-09 12:24:48 +0530

    created login screen

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,14 @@
 import Header from "./components/Header";
 import { Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
-import { Login } from "./pages/Login";
+import Login from "./pages/Login";
 import { Signup } from "./pages/Signup";
 import { Chat } from "./pages/Chat";
 import { NotFound } from "./pages/NotFound";
-import { UserAuth } from "./context/AuthContext";
+import { useAuth } from "./context/AuthContext";
 
 function App() {
-  console.log(UserAuth()?.isLoggedIn);
+  const auth = useAuth();
   return (
     <main>
       <Header />
--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -21,7 +21,7 @@ const AuthContext = createContext<UserAuth | null>(null);
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
   const [user, setUser] = useState<User | null>(null);
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   useEffect(() => {
     // fetch if the user's cookies are valid then skip login
@@ -41,4 +41,4 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 };
 
-export const UserAuth = () => useContext(AuthContext);
+export const useAuth = () => useContext(AuthContext);
